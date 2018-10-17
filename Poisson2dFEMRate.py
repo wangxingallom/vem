b628235b70c53127755c5013c8bf463284790de1
Huayi Wei <weihuayi@xtu.edu.cn>
2018-10-17 20:19:39 +0800

    update

--- Poisson2dFEMRate.py
+++ Poisson2dFEMRate.py
@@ -13,51 +13,44 @@ from fealpy.functionspace.lagrange_fem_space import LagrangeFiniteElementSpace
 
 
 
-pde = CosCosData()
-tmesh = unitcircledomainmesh(0.4, meshtype='tri')
-pmesh = tri_to_polygonmesh(tmesh,2) 
-
 maxit = 3
+pde = CosCosData()
+h = 0.1
+n = 4
+tmesh = unitcircledomainmesh(h, meshtype='tri')
+pmesh = tri_to_polygonmesh(tmesh, n) 
+errorMatrix = np.zeros((8, maxit), dtype=np.float)
 Ndof = np.zeros((maxit,), dtype=np.int)
-ps = [1]
-q  = [5]
-#l2
-integrator = tmesh.integrator(5)
-fem = PoissonFEMModel(pde, tmesh, 1, integrator)
-vem = PoissonVEMModel(pde, pmesh, 1, integrator)
-fem.solve()
-vem.solve()
-uv = vem.uh
-uf = fem.uh
-uI = fem.uI
-u = vem.uI
-isBdNode = tmesh.ds.boundary_node_flag()
-isBdNode1 = pmesh.ds.boundary_node_flag()
-e_f = uf[~isBdNode] - uI[~isBdNode]
-e_v = uv[~isBdNode1] - u[~isBdNode1]
-#L2
-femspace = LagrangeFiniteElementSpace(tmesh, 1) 
-uhv = femspace.function()
-NN = tmesh.number_of_nodes()
-uhv[:] = vem.uh[:NN]
-u = fem.pde.solution
-L2 = fem.integralalg.L2_error(u, uhv)
-#H1
-gu = fem.pde.gradient
-guh = uhv.grad_value
-H1 = fem.integralalg.L2_error(gu, guh)
-#error
-errorMatrix = np.zeros((2, maxit), dtype=np.float)
-j=0
-i=0    	
-errorMatrix[j, i] = np.sqrt(np.mean(e_f**2))
-errorMatrix[j+1, i] = np.sqrt(np.mean(e_v**2))
-errorMatrix[j, i+1] = fem.get_L2_error()
-errorMatrix[j+1, i+1] = L2
-errorMatrix[j, i+2] = fem.get_H1_error()
-errorMatrix[j+1, i+2] = H1
+for i in range(maxit):
+    fem = PoissonFEMModel(pde, tmesh, 1, tmesh.integrator(5))
+    vem = PoissonVEMModel(pde, pmesh, 1, pmesh.integrator(5))
+    fem.solve()
+    vem.solve()
+    u0 = vem.uh
+
+    u1 = fem.uh
+    uI = fem.uI
+    u2 = fem.femspace.function()
+    N = u2.shape[0]
+    u2[:] = u0[:N]
+
+    isBdNode = tmesh.ds.boundary_node_flag()
+    errorMatrix[0, i] = np.sqrt(np.mean((uI - u1)**2))
+    errorMatrix[1, i] = np.sqrt(np.mean((uI - u2)**2))
+
+    errorMatrix[2, i] = fem.get_L2_error()
+    errorMatrix[3, i] = fem.get_L2_error(u2)
+
+    errorMatrix[4, i] = fem.get_H1_error()
+    errorMatrix[5, i] = fem.get_H1_error(u2)
+
+    errorMatrix[6, i] = np.max(np.abs(uI - u1)) 
+    errorMatrix[7, i] = np.max(np.abs(uI - u2)) 
+    
+    if i < maxit -1:
+        h /= 2
+        tmesh = unitcircledomainmesh(h, meshtype='tri')
+        pmesh = tri_to_polygonmesh(tmesh, n) 
 
 print(errorMatrix)
-
-
 
